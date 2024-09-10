8a9bf767529a95e6c2436d5fcec12edcb377a1ad
shiomar1108 <s.salazarc@uniandes.edu.co>
2024-09-09 20:50:57 -0600

    Update monitor.py

--- Semana6/IOTMonitoringServer-main/control/monitor.py
+++ Semana6/IOTMonitoringServer-main/control/monitor.py
@@ -105,7 +105,7 @@ def start_cron():
     Inicia el cron que se encarga de ejecutar la función analyze_data cada 5 minutos.
     '''
     print("Iniciando cron...")
-    schedule.every(5).minutes.do(analyze_data)
+    schedule.every(1).minutes.do(analyze_data)
     print("Servicio de control iniciado")
     while 1:
         schedule.run_pending()
